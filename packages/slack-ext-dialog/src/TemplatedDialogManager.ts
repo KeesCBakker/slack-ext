6d17efd251ad258c53b6f93e3868a98b3d8e3936
Kees C. Bakker <c.bakker@gmx.net>
2019-06-23 20:38:35 +0200

    Made template public.

--- packages/slack-ext-dialog/src/TemplatedDialogManager.ts
+++ packages/slack-ext-dialog/src/TemplatedDialogManager.ts
@@ -2,30 +2,30 @@ import { createJsonHandlebars } from "handlebars-a-la-json"
 import { NamedTemplateParser } from "handlebars-dir"
 import { TemplatedDialog } from "./TemplatedDialog";
 import { ISlackMessageAdapter, ISlackApi, IActionCondition } from "./definitions";
-
+import { IHandlebars } from "handlebars-a-la-json/dist/makeHandlebarsBehave";
 
 export class TemplatedDialogManager{
     
     private _slackMessageAdapter: ISlackMessageAdapter;
     private _dialogs = new Array<TemplatedDialog>();
-    private _templateParser: NamedTemplateParser;
     private _slackApi: ISlackApi;
 
-    public jsonHandlebars: any;
+    public jsonHandlebars: IHandlebars;
+    public templateParser: NamedTemplateParser;
 
     constructor(templateDirectory: string, slackApi: ISlackApi, slackMessageAdapter: ISlackMessageAdapter){
         this.jsonHandlebars = createJsonHandlebars();
         this._slackApi = slackApi;
         this._slackMessageAdapter = slackMessageAdapter;
 
-        this._templateParser = new NamedTemplateParser(this.jsonHandlebars);
-        this._templateParser.loadTemplatesFromDirectorySync(templateDirectory);
+        this.templateParser = new NamedTemplateParser(this.jsonHandlebars);
+        this.templateParser.loadTemplatesFromDirectorySync(templateDirectory);
     }
 
     public createDialog(id: string, templateName: string, triggers: Array<IActionCondition>){
         const dialog = new TemplatedDialog(
             id, 
-            this._templateParser, 
+            this.templateParser, 
             templateName, 
             this._slackApi, 
             this._slackMessageAdapter,
